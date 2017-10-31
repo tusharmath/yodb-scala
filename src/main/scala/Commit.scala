e09ec6e2259740c15fa4fc5d6dc5691ded908778
Tushar Mathur <tusharmath@gmail.com>
2017-10-31 06:58:06 +0530

    refactor(commit): update types

--- src/main/scala/Commit.scala
+++ src/main/scala/Commit.scala
@@ -1,36 +1,31 @@
 import java.security.MessageDigest
 
 
-class Commit(parent: Int, content: String) {
 
-  val DIGEST_ALGORITHM = "MD5"
-  override def toString: String = {
-    parent.toString + Commit.SEPARATOR + content
-  }
+case class Commit(parent: String, data: String) {
+  require(parent.length == 32, "Invalid length of parent digest")
+  require(data.length <= 10 * 4 * 1024, "Invalid parent digest")
 
-  def digest: String = {
-    MessageDigest.getInstance(DIGEST_ALGORITHM)
-      .digest(toString.getBytes)
-      .map("%02x".format(_)).mkString
-  }
-
-  def dir: String = {
-    digest.slice(0, 2)
-  }
-
-  def file: String = {
-    digest.slice(2, this.digest.length)
-  }
+  private val DIGEST_ALGORITHM = "MD5"
+  def digest: String = MessageDigest.getInstance(DIGEST_ALGORITHM)
+    .digest(toString.getBytes)
+    .map("%02x" format _)
+    .mkString
+  def dir: String = digest.take(2)
+  def file: String = digest.drop(2)
+  override def toString: String = parent ++ data.toString
 }
 
 object Commit {
-  val SEPARATOR = "\n"
-  def apply(buffer: String): Commit = {
-    val tokens = buffer.split(SEPARATOR)
-    val parent = tokens.head.toInt
-    val content = tokens.tail.mkString(SEPARATOR)
-    new Commit(parent, content)
-  }
-  def apply[T](parent: Int, content: T): Commit = new Commit(parent, content.toString)
-}
+  val MAX_DATA_LENGTH: Int = 1024 * 10* 4 // 40kb bytes
 
+  def bytesToHex(bytes: Array[Byte]): String = bytes
+    .map("%02x" format _)
+    .mkString
+
+  def from(string: String): Commit = {
+    val parent = string.slice(0, 32)
+    val data = string.slice(32, string.length)
+    Commit(parent, data)
+  }
+}
--- src/test/scala/CommitTest.scala
+++ src/test/scala/CommitTest.scala
@@ -2,26 +2,49 @@
 import org.scalatest.FunSpec
 
 class CommitTest extends FunSpec {
+  private val TEST_DATA = "ALPHA"
+  private val TEST_DIGEST = "5126af838075857d499b950bc6b46c4b"
+  private val TEST_PARENT = "002101f8721e5c78d9f30d87f3fa4c97"
+
+  private case class WooDoo(a: Int, b: Int)
+
+  describe("constructor()") {
+    it("should throw") {
+      assertThrows[IllegalArgumentException](Commit("120", TEST_DATA))
+      assertThrows[IllegalArgumentException](Commit(TEST_DIGEST, 0
+        .to(Commit.MAX_DATA_LENGTH + 1)
+        .map(_ => "A")
+        .mkString)
+      )
+    }
+  }
   describe("toString()") {
     it("should be serializable") {
-      case class WooDoo(a: Int, b: Int)
-      val commit = Commit(120, WooDoo(1, 2))
-      val actual = Commit(commit.toString())
+      val commit = Commit(TEST_PARENT, TEST_DATA)
+      val actual = Commit.from(commit.toString())
       assert(actual.digest === commit.digest)
     }
   }
 
   describe("file") {
     it("should skip the first two letters") {
-      val commit = Commit(100, "ALPHA")
-      assert(commit.file === "79d784cb22aa7d5468ab177c1f7dcc")
+      val commit = Commit(TEST_PARENT, TEST_DATA)
+      assert(commit.file === TEST_DIGEST.slice(2, 32))
     }
   }
 
   describe("dir") {
     it("should pick the first two letters") {
-      val commit = Commit(100, "ALPHA")
-      assert(commit.dir === "2c")
+      val commit = Commit(TEST_PARENT, TEST_DATA)
+      assert(commit.dir === TEST_DIGEST.slice(0, 2))
+    }
+  }
+
+  describe("digest") {
+    it("should return the md5") {
+      val commit = Commit(TEST_PARENT, TEST_DATA)
+      println(commit.digest)
+      assert(commit.digest === TEST_DIGEST)
     }
   }
 }
